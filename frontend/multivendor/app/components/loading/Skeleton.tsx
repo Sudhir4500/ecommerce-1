3b46ba3a3fcd16757d848f9727a8a9a87f477b72
sudhir4500 <sudhirsharma4500@gmail.com>
2025-02-26 09:43:06 +0545

    skeleton responsive

--- frontend/multivendor/app/components/loading/Skeleton.tsx
+++ frontend/multivendor/app/components/loading/Skeleton.tsx
@@ -4,7 +4,10 @@ import React from "react";
 const SkeletonProductCard: React.FC = () => {
   return (
     <div className="border border-gray-300 rounded-xl p-4 animate-pulse">
-      <div className="w-[250px] h-[250px] bg-gray-200 rounded-lg"></div>
+      {/* Image placeholder */}
+      <div className="w-full h-48 sm:h-64 bg-gray-200 rounded-lg"></div>
+
+      {/* Text placeholders */}
       <div className="mt-4 space-y-2">
         <div className="h-4 bg-gray-200 rounded w-3/4"></div>
         <div className="h-4 bg-gray-200 rounded w-1/2"></div>
