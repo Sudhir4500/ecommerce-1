9019551061d301148d427f25ce142009871ee40c
sudhir4500 <sudhirsharma4500@gmail.com>
2025-03-16 19:35:24 +0545

    social auth & payment integration

--- backend/build_files.sh
+++ backend/build_files.sh
@@ -1,3 +1,5 @@
-# build_files.sh
-python -m pip install -r requirements.txt
-python manage.py collectstatic --noinput
+#!/bin/bash
+pip3 install -r requirements.txt
+python3 backend/manage.py migrate --noinput
+python3 backend/manage.py collectstatic --noinput
+ls -la staticfiles  # Debug: Should show staticfiles in the project root
