8ecbcc440c5c9a52d672a2f2eeb4d9b27661ca88
sudhir4500 <sudhirsharma4500@gmail.com>
2025-03-16 19:10:16 +0545

    social auth & payment integration

--- backend/build_files.sh
+++ backend/build_files.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-pip3 install -r requirements.txt
-python3 manage.py makemigrations
-python3 manage.py migrate --noinput
-python3 manage.py collectstatic --noinput
+# build_files.sh
+python -m pip install -r requirements.txt
+python manage.py collectstatic --noinput
