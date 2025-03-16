c088b5bec4735c62ecdaa5f8cfece9bbbdf5307c
sudhir4500 <sudhirsharma4500@gmail.com>
2025-03-16 19:05:39 +0545

    social auth & payment integration

--- backend/build_files.sh
+++ backend/build_files.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Install dependencies
 pip3 install -r requirements.txt
-# Run Django collectstatic to gather static files
+python3 manage.py makemigrations
+python3 manage.py migrate --noinput
 python3 manage.py collectstatic --noinput
